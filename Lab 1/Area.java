10e2d4237d35412191c48594845d9ca2e0fad924
FalduNevil327 <96609416+FalduNevil327@users.noreply.github.com>
2022-02-22 10:49:49 +0530

    Add files via upload

---
+++ Lab 1/Area.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Area{
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter R:");
+		int r =input.nextInt();
+		System.out.println("Area="+(3.14*(r*r)));
+
+	}
+}
