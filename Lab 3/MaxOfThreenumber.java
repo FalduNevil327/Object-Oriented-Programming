34320c059ba143bdd92cdd672f78e3aedbc80235
FalduNevil327 <96609416+FalduNevil327@users.noreply.github.com>
2022-05-10 23:25:53 +0530

    Add files via upload

---
+++ Lab 3/MaxOfThreenumber.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class MaxOfThreenumber{
+
+	public static void main(String args[])
+	{
+		System.out.print("----------Enter Number------");
+		Scanner s=new Scanner(System.in);
+		
+		System.out.print("n1 = ");
+		int n1=s.nextInt();
+
+		System.out.print("n2 = ");
+		int n2=s.nextInt();
+
+		System.out.print("n3 = ");
+		int n3=s.nextInt();
+		System.out.print("Maximum number = ");
+
+		if(n1>n2 && n1>n3)
+		{
+			System.out.print(n1);
+		}
+		else if(n2>n3)
+		{
+			System.out.print(n2);
+		}	
+		else
+		{
+			System.out.print(n3);
+		}
+
+	}
+}
---
+++ Lab 3/Result.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Result{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+
+		System.out.print("Phy Marks : ");
+		float phy = s.nextFloat();
+
+		System.out.print("Sci Marks : ");
+		float sci = s.nextFloat();
+
+		System.out.print("Math Marks : ");
+		float math = s.nextFloat();
+
+		System.out.print("Com Marks : ");
+		float com = s.nextFloat();
+
+		System.out.print("eng Marks : ");
+		float eng = s.nextFloat();
+
+		float total;
+		float avg;
+
+		total = phy + sci + math + com + eng;
+		avg = total/5;
+
+        System.out.println("-----------Result Board----------");
+
+		if(avg>=70.00)
+		{
+			System.out.print("Distinction");
+		}
+		else if(avg>=60.00)
+		{
+			System.out.print("First Class");
+		}
+		else if(avg>=50.00)
+		{
+			System.out.print("Second Class");
+		}
+		else if(avg>=40.00)
+		{
+			System.out.print("Pass Class");
+		}
+		else
+		{
+			System.out.print("Fail");
+		}
+		
+	}
+}
